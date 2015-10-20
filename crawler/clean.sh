c96612bca2c4c34448330ca9ee163c12de19f4c0
Bernardo Rufino <bermonruf@gmail.com>
2015-10-20 01:29:18 -0200

    Fix clean.sh script and directory referral

--- crawler/clean.sh
+++ crawler/clean.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-rm out/data.idx 2> /dev/null
-rm out/data.json 2> /dev/null
-rm log/* 2> /dev/null
+DIR=$(dirname $0)
+
+rm ${DIR}/out/data.idx 2> /dev/null
+rm ${DIR}/out/data.json 2> /dev/null
+rm ${DIR}/log/* 2> /dev/null
 exit 0
