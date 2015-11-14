8b1c2f650558ea07fca9a8b44acf0c733b153e42
Bernardo Rufino <bermonruf@gmail.com>
2015-11-14 03:57:08 -0200

    Add train.py script to train the classifier

---
+++ classifier/train.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+
+from utils import read_dataset, ensure_directory
+from text_processor import TextProcessor
+from classifier import Classifier
+
+
+parser = argparse.ArgumentParser(prog='classify',
+                                 description="""Classifies an unclassified text""")
+
+parser.add_argument('data', help="The data file in json format")
+
+parser.add_argument('-o', '--output', default=os.path.join('out', 'classifier.dat'),
+                    help="Output file where precomputed data for the classifier will be put. Defaults to "
+                         "out/classifier.dat")
+
+
+def main():
+    args = parser.parse_args()
+    data_json = read_dataset(args.data)
+
+    processor = TextProcessor()
+    classifier = Classifier(processor)
+    classifier.train(data_json)
+
+    serialized_classifier = classifier.dump()
+
+    ensure_directory(args.output)
+    with open(args.output, 'w') as f:
+        f.write(serialized_classifier)
+        f.write(os.linesep)
+
+
+if __name__ == '__main__':
+    exit(main())
