e0913f2206e34611c297cd5ac5b938ce66807f98
Bernardo Rufino <bermonruf@gmail.com>
2015-11-14 02:11:32 -0200

    Add text processor

--- classifier/text_processor.py
+++ classifier/text_processor.py
@@ -23,3 +23,40 @@ class TextProcessor(object):
             example['tokens'] = self.process_text(example['content'])
             for token in example['tokens']:
                 self._vocab.add(token)
+
+    def _process_text(self, text):
+
+        # Unicode to ascii
+        text = text.decode('utf-8')
+        text = unicodedata.normalize('NFD', text)
+        text = text.encode('ascii', 'ignore')
+
+        # Lower case
+        text = text.lower()
+
+        # Compact spaces
+        text = re.sub('\s+', ' ', text)
+
+        # Time and dates
+        text = re.sub('\d+min([^\d]|$)', '<time>\\1', text)
+        text = re.sub('\d?\d:\d\d([^\d]|$)', '<time>\\1', text)
+        text = re.sub('\d?\dh(?:\d\d)?([^\d]|$)', '<time>\\1', text)
+        text = re.sub('\d\d/\d\d(?:/\d\d(?:\d\d)?)?', '<date>', text)
+
+        # Drop specials, only letters, numbers and spaces
+        text = re.sub('[^\w\s]', '', text)
+
+        # Numbers and money
+        text = re.sub('\d+', '<number>', text)
+        text = re.sub('<number>bi\s', '<money> ', text)
+        text = re.sub('<number>\sreais', '<money>', text)
+        text = re.sub('<number>\sdolares', '<money>', text)
+        text = re.sub('\sr\s<number>', ' <money>', text)
+        text = re.sub('\su\s<number>', ' <money>', text)
+
+        # Trim
+        text.strip()
+
+        return text
+
+
